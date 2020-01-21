46bb776a3063f9ed64b9fe26de84795352a552f7
metalwarrior665 <lukaskrivka@gmail.com>
2020-01-21 01:28:05 +0100

    added section

--- src/compute-units-notification.js
+++ src/compute-units-notification.js
@@ -10,12 +10,13 @@ const sendMail = async (CUs, limit, notificationEmails, runId) => {
 };
 
 module.exports = async (stopAfterCUs, notifyAfterCUs, notificationEmails, notifyAfterCUsPeriodically, notificationState) => {
-    console.log('Checking if to send notifications...');
+    console.log('NOTIFICATIONS --- Checking if to send notifications...');
     const { actorId, actorRunId } = Apify.getEnv();
     const { stats } = await Apify.client.acts.getRun({ actId: actorId, runId: actorRunId });
     const CUs = stats.computeUnits;
     if (notifyAfterCUsPeriodically) {
         const { next } = notificationState;
+        console.log(`NOTIFICATIONS --- Actor reached ${CUs} which is more than notifyAfterCUsPeriodically: ${next}. Sending notification email`);
         if (CUs >= next) {
             await sendMail(CUs, next, notificationEmails, actorRunId);
             notificationState.next += notifyAfterCUsPeriodically;
@@ -23,13 +24,13 @@ module.exports = async (stopAfterCUs, notifyAfterCUs, notificationEmails, notify
         }
     }
     if (!notificationState.wasNotified && notifyAfterCUs && CUs >= notifyAfterCUs) {
-        console.log(`Actor reached ${CUs} which is more than notifyAfterCUs: ${stopAfterCUs}. Sending notification email`);
+        console.log(`NOTIFICATIONS --- Actor reached ${CUs} which is more than notifyAfterCUs: ${notifyAfterCUs}. Sending notification email`);
         await sendMail(CUs, notifyAfterCUs, notificationEmails, actorRunId);
         notificationState.wasNotified = true;
         await Apify.setValue('NOTIFICATION-STATE', notificationState);
     }
     if (stopAfterCUs && CUs >= stopAfterCUs) {
-        console.log(`Actor reached ${CUs} which is more than stopAfterCUs: ${stopAfterCUs}. Exiting actor.`);
+        console.log(`NOTIFICATIONS --- Actor reached ${CUs} which is more than stopAfterCUs: ${stopAfterCUs}. Exiting actor.`);
         process.exit(0);
     }
 };
